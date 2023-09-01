db87e68a7b550d06eaa14b048adff4274143b991
SergeyKarachevtcev <estel141081@gmail.com>
2023-09-01 15:12:35 +0300

    Основной ленд, без мобильной версии

---
+++ script/skript.js
@@ -0,0 +1,39 @@
+/* search menu */
+
+function openModal() {
+	document.getElementById("overlay").style.top = "0px";
+}
+
+function closeModal() {
+	document.getElementById("overlay").style.top = "-100vh";
+}
+
+
+/* swiper */
+
+var swiper = new Swiper(".mySwiper", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+
+// Tabs
+
+function openTab(event, tabName) {
+var i, tabcontent, tablinks;
+// Скрываем все элементы с классом "tab-content"
+tabcontent = document.getElementsByClassName("tab-content");
+for (i = 0; i < tabcontent.length; i++) {
+tabcontent[i].style.display = "none";
+}
+// Удаляем класс "active" со всех кнопок с классом "tablinks"
+tablinks = document.getElementsByClassName("tab-link");
+for (i = 0; i < tablinks.length; i++) {
+tablinks[i].classList.remove("active");
+}
+// Отображаем текущую вкладку и добавляем класс "active" к кнопке, которая открывает эту вкладку
+document.getElementById(tabName).style.display = "block";
+event.currentTarget.classList.add("active");
+}
